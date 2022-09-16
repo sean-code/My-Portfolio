e09238f2aaa83155893b035de3769d526ab887ba
sean-code <john.nganga@student.moringaschool.com>
2022-09-16 18:25:39 +0300

    Add internal js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let typed = new Typed(".typing", {
-    strings:['John-Sean Nganga'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
-})
+// let typed = new Typed(".typing", {
+//     strings:['John-Sean Nganga'],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     loop: true
+// })
